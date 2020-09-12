eb3b8f1554f1cd1169c789f05f6de12c3c92cb07
happiness <melow2@naver.com>
2020-09-12 15:45:39 +0900

    [DEV] main UI

--- app/src/main/java/com/khs/movieclient/view/MainActivity.kt
+++ app/src/main/java/com/khs/movieclient/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.khs.movieclient
+package com.khs.movieclient.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.khs.movieclient.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
